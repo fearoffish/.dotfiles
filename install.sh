727015f5bec283da95ea7c18fb429f5bee5d7e47
Jamie van Dyke <me@fearof.fish>
2025-11-02 23:52:24 +0000

    Update .gitconfig Update .ssh/config Update install.sh Add .config/lazygit/config.yml Add .config/fish

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ install_1password_cli() {
             curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
             sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
             curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-            sudo apt update && sudo apt install 1password-cli
+            sudo apt update && sudo apt install 1password 1password-cli
             ;;
         *)
             log_warning "Unsupported OS for 1Password CLI: $OS"
