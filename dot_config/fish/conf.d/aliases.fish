8ac68e63845d52999265b5808fd3064f0742b769
Jamie van Dyke <me@fearof.fish>
2025-12-11 11:18:44 +0000

    Add a simple aws exec alias

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -8,3 +8,10 @@ alias bi "bundle install"
 # Directories
 alias cdw "cd ~/a/nevaya"
 alias cdnev "cd ~/a/nevaya/nev"
+
+alias awsexec 'AWS_REGION=eu-west-1 AWS_PROFILE=staging aws ecs execute-command \
+        --cluster nv-dev-euw1-ecs-admin \
+        --container genieacs \
+        --interactive \
+        --command "/bin/bash" \
+        --task '
